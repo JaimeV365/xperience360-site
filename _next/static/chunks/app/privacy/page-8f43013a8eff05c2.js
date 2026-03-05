8b0893550b7c4a4dd9dfa18b83500cc9ffdf233d
Jaime <jaime-valle@outlook.com>
2026-03-05 09:11:01 +0000

    Initial static site deploy

---
+++ _next/static/chunks/app/privacy/page-8f43013a8eff05c2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[385,160,41],{5339:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=5339)}),_N_E=n.O()}]);
